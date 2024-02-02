d565b96c667c2bd78b2c0a7ceebc4ee216141636
Omersidd <omersidd746@gmail.com>
2024-02-02 18:00:35 +0500

    My first commit

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -16,3 +16,15 @@ class IndexTestCase(TestCase):
     def test_index(self):
         rv = self.client.get('/')
         self.assert200(rv)
+
+    def test_function_with_bug(self):
+        result = my_buggy_function(5)  # Intentionally call a function with a bug
+        assert result == 10, "Incorrect result returned"
+
+    def test_function_with_bug():
+        result = my_buggy_function(5)  # Intentionally call a function with a bug
+        assert result == 10, "Incorrect result returned"
+
+    def test_coding_standard_violation():
+        with pytest.raises(SyntaxError):
+            exec("x = 1  # Missing whitespace after #")  # Intentional syntax error
